6c3cc7aa731ef1cd1069f562f838ab1313e7e3a6
Luofan Chen <clfbbn@gmail.com>
2023-04-11 07:10:55 +0000

    bench: save bench config to result dir

--- bench.sh
+++ bench.sh
@@ -126,4 +126,6 @@ done     # profile
 
 rm result/latest || /bin/true
 ln -s $LOCAL_DIR/"$result_dir" $LOCAL_DIR/"result/latest"
+mkdir -p $LOCAL_DIR/"$result_dir"/configuration
+echo "$CONFIG_BENCH_PROFILE" > $LOCAL_DIR/"$result_dir"/configuration/profile
 ask_for_tag
