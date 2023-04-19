865b297dc0997b0850960dc875e0c56c78e048b7
Luofan Chen <clfbbn@gmail.com>
2023-04-19 06:27:46 +0000

    update
    - use tab
    - mysql unification

--- bench.sh
+++ bench.sh
@@ -12,7 +12,7 @@ set -e
 
 # print debug information print
 if [[ ${DEBUG} -eq 1 ]]; then
-    set -x
+	set -x
 fi
 
 # Export common benchmark variables
@@ -29,16 +29,16 @@ export result_dir="result/result_${date_suffix}"
 # Read from config
 spec_config=setup_$(hostname)
 if [[ -f "config/${spec_config}" ]]; then
-    # shellcheck source=common/setup_bbnpm
-    source "config/${spec_config}"
+	# shellcheck source=common/setup_bbnpm
+	source "config/${spec_config}"
 else
-    # shellcheck source=common/setup
-    source "config/setup"
+	# shellcheck source=common/setup
+	source "config/setup"
 fi
 
 # The arguments are the profile to run
 if [[ $# -gt 0 ]]; then
-    CONFIG_BENCH_PROFILE="$*"
+	CONFIG_BENCH_PROFILE="$*"
 fi
 
 # Import util functions
@@ -48,13 +48,13 @@ source utils/fn_zoned
 source utils/fn_tags
 
 function on_sigint {
-    # Ask if we want to delete this directory
-    ask_for_deletion
+	# Ask if we want to delete this directory
+	ask_for_deletion
 
-    # Cleanup if needed
-    sudo service mysql stop
+	# Cleanup if needed
+	sudo service mysql stop
 
-    exit 0
+	exit 0
 }
 trap 'on_sigint' SIGINT
 
@@ -65,71 +65,71 @@ trap 'on_sigint' SIGINT
 #  - $1: Benchmark list
 ##
 function init_profile {
-    echo "off" | sudo tee /sys/devices/system/cpu/smt/control || /bin/true
+	echo "off" | sudo tee /sys/devices/system/cpu/smt/control || /bin/true
 
-    # TODO
+	# TODO
 }
 
 function run_prepare {
-    zoned_format_"$1"
-    zoned_mount_"$1"
+	zoned_format_"$1"
+	zoned_mount_"$1"
 }
 
 setup_loop
 
 for profile in $CONFIG_BENCH_PROFILE; do
-    # Every profile has its own directory
-    dir_name="${result_dir}/$profile"
-    mkdir -p "${dir_name}"
+	# Every profile has its own directory
+	dir_name="${result_dir}/$profile"
+	mkdir -p "${dir_name}"
 
-    # shellcheck source=profile/main
-    source profile/"$profile"
-    inline_func profile/"$profile" > "$dir_name/profile"
+	# shellcheck source=profile/main
+	source profile/"$profile"
+	inline_func profile/"$profile" >"$dir_name/profile"
 
-    init_profile "$setting_bench"
+	init_profile "$setting_bench"
 
-    for bench in $setting_bench; do
-        # shellcheck source=bench/fio/fn_fio
-        source "bench/${bench}/fn_${bench}"
-        mkdir -p "$dir_name/$bench"
-        if [[ -n $(echo "$bench" | grep raw || /bin/true) ]]; then
-            zoned_cleanup
-            bench_single_main "$dir_name/$bench/$bench-raw"
-        else
-            for bench_fs in $setting_fs; do
-                if [[ ${bench_fs} == "dm-zoned" ]]; then
-                    bench_zoned_fs=${setting_dmzoned_fs}
-                else
-                    bench_zoned_fs="${bench_fs}"
-                fi
+	for bench in $setting_bench; do
+		# shellcheck source=bench/fio/fn_fio
+		source "bench/${bench}/fn_${bench}"
+		mkdir -p "$dir_name/$bench"
+		if [[ -n $(echo "$bench" | grep raw || /bin/true) ]]; then
+			zoned_cleanup
+			bench_single_main_${bench} "$dir_name/$bench/$bench-raw"
+		else
+			for bench_fs in $setting_fs; do
+				if [[ ${bench_fs} == "dm-zoned" ]]; then
+					bench_zoned_fs=${setting_dmzoned_fs}
+				else
+					bench_zoned_fs="${bench_fs}"
+				fi
 
-                for bench_zfs in $bench_zoned_fs; do
-                    export BENCH_DIR=${LOCAL_DIR}/bench/${bench}
+				for bench_zfs in $bench_zoned_fs; do
+					export BENCH_DIR=${LOCAL_DIR}/bench/${bench}
 
-                    zoned_format_"${bench_fs}" $bench_zfs
-                    zoned_mount_"${bench_fs}" $bench_zfs
+					zoned_format_"${bench_fs}" $bench_zfs
+					zoned_mount_"${bench_fs}" $bench_zfs
 
-                    bench_single_main "$dir_name/$bench/$bench-${bench_fs}_${bench_zfs}"
+					bench_single_main_${bench} "$dir_name/$bench/$bench-${bench_fs}_${bench_zfs}"
 
-                    zoned_cleanup
-                done # for bench_zfs in $bench_zoned_fs
-            done     # fs
-        fi           # if raw
+					zoned_cleanup
+				done # for bench_zfs in $bench_zoned_fs
+			done  # fs
+		fi     # if raw
 
-        # this bench is over, see if we want to process
-        # Test whether or not the file fn_${bench}_process exists
-        if [[ -f "bench/${bench}/fn_${bench}_process" ]]; then
-            # shellcheck source=bench/fio/fn_fio_process
-            source "bench/${bench}/fn_${bench}_process"
-            post_process "$dir_name/$bench"
-            unset post_process
-        fi
-    done # bench
-done     # profile
+		# this bench is over, see if we want to process
+		# Test whether or not the file fn_${bench}_process exists
+		if [[ -f "bench/${bench}/fn_${bench}_process" ]]; then
+			# shellcheck source=bench/fio/fn_fio_process
+			source "bench/${bench}/fn_${bench}_process"
+			post_process "$dir_name/$bench"
+			unset post_process
+		fi
+	done # bench
+done  # profile
 
 rm result/latest || /bin/true
 if [[ -n $CONFIG_USER ]]; then
-    chown -R $CONFIG_USER:$CONFIG_USER "$LOCAL_DIR"/result
+	chown -R $CONFIG_USER:$CONFIG_USER "$LOCAL_DIR"/result
 fi
 ln -s $LOCAL_DIR/"$result_dir" $LOCAL_DIR/"result/latest"
 ask_for_tag
--- tools/fio_bw_tbl.sh
+++ tools/fio_bw_tbl.sh
@@ -3,31 +3,31 @@
 dir="$1"
 
 if [[ -z $dir ]]; then
-    echo "Usage: $0 <dir>"
-    exit 1
+	echo "Usage: $0 <dir>"
+	exit 1
 fi
 
 if [[ ! -d $dir ]]; then
-    echo "Directory $dir does not exist"
-    exit 1
+	echo "Directory $dir does not exist"
+	exit 1
 fi
 
 # Use the remaining arguments (if exist) for query
 if [[ $# -gt 1 ]]; then
-    query=( ${@:2} )
+	query=(${@:2})
 fi
 
 grep_str=$(grep 'bw=' "$dir"/**/* | tr -s ' ' | sort -h)
 for q in "${query[@]}"; do
-    grep_str=$(echo "$grep_str" | grep -E "[_-]$q[-.]")
+	grep_str=$(echo "$grep_str" | grep -E "[_-]$q[-.]")
 done
 
 type=$(echo "$grep_str" | cut -d ' ' -f 1 | rev | cut -d '/' -f 1 | rev | sed 's/fio-//' | sed 's/\.log//' | sed 's/\://')
 bw=$(echo "$grep_str" | cut -d ' ' -f 3 | sed 's/bw=//' | sed 's/MiB\/s//')
 
-readarray -t type_arr <<< "$type"
-readarray -t bw_arr <<< "$bw"
+readarray -t type_arr <<<"$type"
+readarray -t bw_arr <<<"$bw"
 
 for i in "${!type_arr[@]}"; do
-    printf "${type_arr[$i]}\t${bw_arr[$i]}\n"
+	printf "${type_arr[$i]}\t${bw_arr[$i]}\n"
 done
